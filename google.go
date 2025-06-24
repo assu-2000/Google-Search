1c9029924a55e094c24a260b40ab5de32abce308
assu-2000 <kayassurance@gmail.com>
2025-06-24 23:14:49 +0100

    Google Search v3.0

--- google.go
+++ google.go
@@ -7,9 +7,17 @@ import (
 )
 
 var (
-	Web   = fakeSearch("Web")
-	Image = fakeSearch("Image")
-	Video = fakeSearch("Video")
+	Web1   = fakeSearch("Web1")
+	Image1 = fakeSearch("Image1")
+	Video1 = fakeSearch("Video1")
+
+	Web2   = fakeSearch("Web2")
+	Image2 = fakeSearch("Image2")
+	Video2 = fakeSearch("Video2")
+
+	Web3   = fakeSearch("Web3")
+	Image3 = fakeSearch("Image3")
+	Video3 = fakeSearch("Video3")
 )
 
 type Search func(string) Result
@@ -22,12 +30,23 @@ func fakeSearch(kind string) Search {
 	}
 }
 
+func First(query string, replicas ...Search) Result {
+	c := make(chan Result)
+	searchReplica := func(i int) { c <- replicas[i](query) }
+
+	for i := range replicas {
+		go searchReplica(i)
+	}
+
+	return <-c
+}
+
 func Google(query string) (results []Result) {
 	c := make(chan Result)
 
-	go func() { r := Web(query); c <- r }()
-	go func() { c <- Image(query) }() // fan-in pattern
-	go func() { c <- Video(query) }()
+	go func() { c <- First(query, Web1, Web2, Web3) }()
+	go func() { c <- First(query, Image1, Image2, Image3) }() // fan-in pattern
+	go func() { c <- First(query, Video1, Video2, Video3) }()
 
 	timeout := time.After(80 * time.Millisecond) // timeout for the whole process
 
